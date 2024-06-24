7459099760747ebe195f86718f4cf06cf5afd260
Ginger Society Admin <admin@gingersociety.org>
2024-06-24 18:13:16 +0530

    feat: added util function to read config file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,60 @@
+use clap::Parser;
+use clap::Subcommand;
+
 mod release_notes;
+mod utils;
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Initialize a releaser config in a project
+    Init,
+    /// Creates a release by bumping the version, creating a git tag and generating CHANGELOG.md file
+    Release,
+}
+
+#[derive(Parser, Debug)]
+#[command(name = "Ginger-Releaser")]
+#[command(about = "A release management CLI tool", long_about = None)]
+#[command(version, long_about = None)]
+struct Args {
+    /// name of the command to run
+    #[command(subcommand)]
+    command: Commands,
+}
 
 fn main() {
-    match release_notes::generate_release_notes() {
-        Err(e) => {
-            println!("Unable to generate {:?}", e);
+    let args = Args::parse();
+
+    let file_path = "releaser.toml"; // Update the path to your TOML file
+
+    let config = utils::read_config(file_path).unwrap();
+    println!(
+        "Version: {}.{}.{}-{}.{}",
+        config.version.major,
+        config.version.minor,
+        config.version.patch,
+        config.version.channel,
+        config.version.revision
+    );
+
+    for reference in config.references {
+        println!(
+            "File: {}, Type: {}, Variable: {}, FileType : {}",
+            reference.file_name, reference.output_type, reference.variable, reference.file_type
+        );
+    }
+
+    match args.command {
+        Commands::Init => {}
+        Commands::Release => {
+            match release_notes::generate_release_notes() {
+                Err(e) => {
+                    println!("Unable to generate {:?}", e);
+                }
+                Ok(_) => {
+                    println!("Generated release notes successfully")
+                }
+            };
         }
-        Ok(_) => {
-            println!("Generated release notes successfully")
-        }
-    };
+    }
 }
---
+++ src/utils.rs
@@ -0,0 +1,95 @@
+use serde::{Deserialize, Serialize};
+use std::{error::Error, fmt, fs, path::Path};
+
+#[derive(Debug)]
+pub enum FileType {
+    Py,
+    Toml,
+    Json,
+    Unknown,
+}
+
+impl FileType {
+    fn from_extension(ext: Option<&str>) -> FileType {
+        match ext {
+            Some("py") => FileType::Py,
+            Some("toml") => FileType::Toml,
+            Some("json") => FileType::Json,
+            _ => FileType::Unknown,
+        }
+    }
+}
+
+impl fmt::Display for FileType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FileType::Py => write!(f, "Py"),
+            FileType::Toml => write!(f, "Toml"),
+            FileType::Json => write!(f, "Json"),
+            FileType::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Version {
+    pub channel: String,
+    pub major: u32,
+    pub minor: u32,
+    pub patch: u32,
+    pub revision: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum OutputType {
+    String,
+    Tuple,
+}
+
+impl fmt::Display for OutputType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            OutputType::String => write!(f, "String"),
+            OutputType::Tuple => write!(f, "Tuple"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Reference {
+    pub file_name: String,
+    #[serde(default = "default_output_type")] // Use a default value function
+    pub output_type: OutputType, // `type` is a reserved keyword in Rust
+    pub variable: String,
+    #[serde(skip, default = "default_file_type")] // This field is not in the TOML file
+    pub file_type: FileType,
+}
+
+fn default_file_type() -> FileType {
+    FileType::Unknown
+}
+
+fn default_output_type() -> OutputType {
+    OutputType::String // Default value is "string"
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub version: Version,
+    pub references: Vec<Reference>,
+}
+
+pub fn read_config(file_path: &str) -> Result<Config, Box<dyn Error>> {
+    let contents = fs::read_to_string(file_path)?;
+    let mut config: Config = toml::from_str(&contents)?;
+
+    for reference in &mut config.references {
+        reference.file_type = FileType::from_extension(
+            Path::new(&reference.file_name)
+                .extension()
+                .and_then(|s| s.to_str()),
+        );
+    }
+
+    Ok(config)
+}
