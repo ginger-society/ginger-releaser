15e66d08354b4598c16e7ba036e04f8079bcf0d4
Ginger Society Admin <admin@gingersociety.org>
2024-06-25 15:52:03 +0530

    chore: version bump to 8.3.0

--- src/release_notes.rs
+++ src/release_notes.rs
@@ -1,3 +1,4 @@
+use chrono::{Datelike, Local}; // Import chrono to handle date and time
 use git2::{Oid, Repository};
 use std::collections::HashMap;
 use std::fs::File;
@@ -13,35 +14,36 @@ pub fn generate_release_notes(
     let repo = Repository::open(".")?;
     let mut tags: HashMap<String, Oid> = HashMap::new();
     let mut release_notes: HashMap<String, Vec<String>> = HashMap::new();
-    let mut sorted_tags: Vec<String> = vec![];
+    let mut sorted_tags: Vec<(String, chrono::NaiveDateTime)> = vec![]; // Change sorted_tags to hold datetime too
+
     // Get all tags in the repository and filter by semantic versioning
     for tag in repo.tag_names(None)?.iter() {
         match tag {
             Some(version) => {
-                tags.insert(
-                    version.to_string().clone(),
-                    repo.refname_to_id(format!("refs/tags/{}", &version).as_str())?,
-                );
-                sorted_tags.push(version.to_string().clone());
-                release_notes.insert(version.to_string().clone(), Vec::new());
+                let tag_name = version.to_string().clone();
+                let tag_id = repo.refname_to_id(format!("refs/tags/{}", &tag_name).as_str())?;
+                tags.insert(tag_name.clone(), tag_id);
+
+                // Get tag date
+                let tag_date = repo.find_commit(tag_id)?.time().seconds();
+                let tag_datetime = chrono::NaiveDateTime::from_timestamp(tag_date, 0);
+
+                sorted_tags.push((tag_name.clone(), tag_datetime));
+                release_notes.insert(tag_name.clone(), Vec::new());
             }
             None => {}
         }
     }
 
-    sorted_tags.sort_by(|a, b| {
-        let semver_a = Version::from_str(a);
-        let semver_b = Version::from_str(b);
-        semver_b.cmp(&semver_a)
-    });
+    sorted_tags.sort_by(|a, b| b.1.cmp(&a.1)); // Sort by datetime in descending order
 
     // Iterate over tags and collect commit messages
-    for (i, tag_name) in sorted_tags.iter().enumerate() {
+    for (i, (tag_name, _)) in sorted_tags.iter().enumerate() {
         let mut revwalk = repo.revwalk()?;
         let mut commit_messages = Vec::new();
 
         if i < sorted_tags.len() - 1 {
-            let next_tag_name = &sorted_tags[i + 1];
+            let next_tag_name = &sorted_tags[i + 1].0;
 
             let start_commit = repo.find_commit(*tags.get(next_tag_name).unwrap())?;
 
@@ -78,72 +80,17 @@ pub fn generate_release_notes(
             .extend(commit_messages);
     }
 
-    // Collect commits since the last tag under "Unreleased commits"
-    let mut revwalk = repo.revwalk()?;
-    let head = repo.head()?;
-
-    // Get the OID (commit ID) of the HEAD commit
-    let head_oid = head
-        .target()
-        .ok_or_else(|| git2::Error::from_str("No commit in HEAD"))?;
-
-    // Find the commit object using the OID
-    let last_commit = repo.find_commit(head_oid)?;
-
-    if let Some(last_tag_name) = sorted_tags.first().cloned() {
-        let end_commit = repo.find_commit(*tags.get(&last_tag_name).unwrap())?;
-        revwalk.push(last_commit.id())?;
-        revwalk.hide(end_commit.id())?;
-        let mut commit_messages = Vec::new();
-
-        for commit_id in revwalk {
-            let commit_id = commit_id?;
-            let commit = repo.find_commit(commit_id)?;
-            let commit_hash = commit.id().to_string();
-            let author = commit.author();
-            let message = commit.message().unwrap_or_default();
-
-            let formatted_message = format!(
-                " - [{}]({}{}) ({}) {}\n",
-                &commit_hash[..10],
-                git_url_prefix,
-                commit_hash,
-                author.name().unwrap_or_default(),
-                message.replace("\n", "\n\t")
-            );
-
-            commit_messages.push(formatted_message);
-        }
-
-        release_notes.insert(String::from(version.formatted()), commit_messages);
-    }
-
     match File::create("CHANGELOG.md") {
         Ok(mut release_notes_file) => {
-            match release_notes.get(&version.formatted()) {
-                Some(notes) => {
-                    match write!(
-                        release_notes_file,
-                        "## {}\n",
-                        String::from(version.formatted())
-                    ) {
-                        Ok(()) => {}
-                        Err(_) => exit(0),
-                    };
-                    for note in notes {
-                        match write!(release_notes_file, "{}", note) {
-                            Ok(()) => {}
-                            Err(_) => exit(0),
-                        };
-                    }
-                }
-                None => {}
-            }
-
-            for tag_name in sorted_tags.iter() {
+            for (tag_name, tag_datetime) in sorted_tags.iter() {
                 match release_notes.get(tag_name) {
                     Some(notes) => {
-                        match write!(release_notes_file, "## {}\n", tag_name) {
+                        match write!(
+                            release_notes_file,
+                            "## {} ({})\n",
+                            String::from(tag_name),
+                            tag_datetime.date() // Get date part only
+                        ) {
                             Ok(()) => {}
                             Err(_) => exit(0),
                         };
--- test_files/__init__.py
+++ test_files/__init__.py
@@ -1,2 +1,2 @@
-VERSION = (8, 2, 3, "final", 0)
-__version__ = "8.2.3"
+VERSION = (8, 3, 0, "final", 0)
+__version__ = "8.3.0"
